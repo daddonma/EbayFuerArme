118607350780ac96eebe6f992a8a6b943c5de4f0
brian.korduan <bkorduan@web.de>
2016-05-11 12:27:06 +0200

    css update new Product

--- newProduct.php
+++ newProduct.php
@@ -1,52 +1,107 @@
-<?php
-session_start();
- $dbConnect = mysqli_connect("localhost", "root", "", "ebayfuerarme") or die (mysqli_error());
- $kategorien = "SELECT KID, Bezeichnung FROM kategorie";
- $result = mysqli_query($dbConnect, $kategorien);
+<html>
+    <head>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="CSS/customCSS.css" />
+    </head>
+    <body> 
+        <div class="container-fluid">
 
-?>
-<link rel="stylesheet" type="text/css" href="Style.css" />
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">WebSiteName</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li> 
+                        <li><a href="#">Page 3</a></li> 
+                    </ul>
+                </div>
+
+            </nav>
 
 
-<form action ="newProduct.php" method="POST">
-    <label for="bezeichnung">Artikelbezeichnung: </label> <input type="text" name="bezeichnung" value=""><br>
-    <label for="kategorie">Kategorie</label><select name="kategorie"> 
-<?php 
-   while ( $row=mysqli_fetch_assoc($result)) { 
-                      echo '<option value="'.$row['KID'].'">'.$row['Bezeichnung'].'</option>'; 
-                  }  
-?> 
-    </select><br>
-    <label for="beschreibung">Artikelbeschreibung: </label> <input style="height:300px" type="textarea" name="beschreibung" value="">
-    <label for="startpreis">Startpreis</label> <input type="text" name="startpreis" value=""><br>
-    <input type="submit" value="Angebot erstellen">
-</form>
-<a href="Home.php">Zurück</a>
-<?php
-if(isset($_POST['kategorie'])) {
-    $bezeichnung = $_POST['bezeichnung'];
-    $kategorie = $_POST['kategorie'];
-    $beschreibung = $_POST['beschreibung'];
-    $starpreis = $_POST['startpreis'];
-    
-    $insert = "INSERT INTO produkte(Bezeichnung, KategorieID, Anbieter, Text, Startpreis) VALUES"
-            ."('".$bezeichnung."', ".$kategorie.", ".$_SESSION['uid'].", '".$beschreibung."', ".$starpreis.");";
-    
+            <?php
+            session_start();
+            $dbConnect = mysqli_connect("localhost", "root", "", "ebayfuerarme") or die(mysqli_error());
+            $kategorien = "SELECT KID, Bezeichnung FROM kategorie";
+            $result = mysqli_query($dbConnect, $kategorien);
+            ?>
+            <link rel="stylesheet" type="text/css" href="Style.css" />
 
-    //echo $insert;
-    if ($dbConnect->query($insert) === TRUE) {
-            echo "Auktion erfolgreich gestartet";             
+            <form action ="newProduct.php" method="POST">
+                <div class="row">
+                    <div class="col-sm-1">
+                        <label for="bezeichnung">Artikelbezeichnung: </label> </div>
+                    <div class="col-sm-11">   <input type="text" name="bezeichnung" value="" class="form-control"><br></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-1">
+                        <label for="kategorie">
+                            Kategorie
+                        </label></div>
+                    <div class="col-sm-11">
+                        <select name="kategorie" class="form-control"> 
+
+
+                            <?php
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo '<option value="' . $row['KID'] . '">' . $row['Bezeichnung'] . '</option>';
+                            }
+                            ?> 
+
+                        </select><br>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-1">
+                        <label for="beschreibung">Artikelbeschreibung:</label> 
+                    </div>
+                    <div class="col-sm-11">
+                        <textarea rows="10"  class="form-control" name="beschreibung">
+                        </textarea>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-1">
+                        <label for="startpreis">Startpreis</label> 
+                    </div>
+                    <div class="col-sm-11">
+                        <input type="text" name="startpreis" value="" class="form-control"><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <input type="submit" value="Angebot erstellen" class="btn btn-primary">
+                    </div>
+                </div>
+            </form>
+            <a href="Home.php">Zurück</a>
+            <?php
+            if (isset($_POST['kategorie'])) {
+                $bezeichnung = $_POST['bezeichnung'];
+                $kategorie = $_POST['kategorie'];
+                $beschreibung = $_POST['beschreibung'];
+                $starpreis = $_POST['startpreis'];
+
+                $insert = "INSERT INTO produkte(Bezeichnung, KategorieID, Anbieter, Text, Startpreis) VALUES"
+                        . "('" . $bezeichnung . "', " . $kategorie . ", " . $_SESSION['uid'] . ", '" . $beschreibung . "', " . $starpreis . ");";
+
+
+                //echo $insert;
+                if ($dbConnect->query($insert) === TRUE) {
+                    echo "Auktion erfolgreich gestartet";
                 } else {
-                        echo "Error";
-                 }
-      
-         
-   
-            
-}
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                    echo "Error";
+                }
+            }
+            ?>
+        </div>
+    </body>
+</html>
 
--- showProduct.php
+++ showProduct.php
@@ -23,7 +23,7 @@
 
             </nav>
             <div class="col-sm-4">
-                <img src="Logo.png" width="150px" height="150px">
+                <img src="platzhalter.svg" class="img-thumbnail">
             </div>
 
             <?php
@@ -79,38 +79,47 @@
                 $kategorie = $row[2];
                 $anbieter = $row[3];
                 $gebot = $row[4];
-            } ?>
-                <div class="col-sm-4">
+            }
+            ?>
+            <div class="col-sm-4">
 
-                    <h1><?php echo $bezeichnung; ?><br></h1>
+                <h1><?php echo $bezeichnung; ?><br></h1>
 
-                    <label>Kategorie: </label> <?php echo $kategorie; ?><br>
-                    <label>Anbieter: </label> <?php echo $anbieter; ?><br>
+                <label>Kategorie: </label> <?php echo $kategorie; ?><br>
+                <label>Anbieter: </label> <?php echo $anbieter; ?><br>
+            </div>
+            <div class="col-sm-4">
+                <div class="row">
+                    <label>EUR: </label> <?php echo $gebot; ?><br><br>
                 </div>
-                <div class="col-sm-4">
-                    <div class="row">
-                        <label>EUR: </label> <?php echo $gebot; ?><br><br>
-                    </div>
 
-                    <div class="row">
-                        <form action="showProduct.php" method="GET">
-                            <label for="gebot">Gebot</label> 
-                    </div>
-                    <div class="row">
-                        <input type="number" name="gebot" class="form-control">
-                    </div>
+                <div class="row">
+                    <form action="showProduct.php" method="GET">
+                        <label for="gebot">Gebot</label> 
+                </div>
+                <div class="row">
+                    <input type="number" name="gebot" class="form-control">
+                </div>
 
-                    <div class="row">
-                        <input name="Artikel" type="hidden" value="<?php echo $artikelnr ?>">
-                        <input type="submit" value="Gebot abgeben" class="btn btn-primary btn-block">
-                    </div>
-                    <div class="row">
-                        <input type="submit" value="Sofort kaufen" class="btn btn-primary btn-block">
-                    </div>
+                <div class="row">
+                    <input name="Artikel" type="hidden" value="<?php echo $artikelnr ?>">
+                    <input type="submit" value="Gebot abgeben" class="btn btn-primary btn-block">
+                </div>
+                <div class="row">
+                    <input type="submit" value="Sofort kaufen" class="btn btn-primary btn-block">
                 </div>
             </div>
-            <a href="home.php">Zurück</a>
-</form>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h4>Beschreibung:</h4>
+                <blockquote>  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+                    </p></blockquote>
+                <a href="home.php">Zurück</a>
+
+            </div>
+        </div>
+    </form>
 
 </body>
 </html>
